788e28e6f0179950166b996fddd6dd9248797eeb
Johnson Page <jwpage@gmail.com>
2017-11-28 12:16:04 +1100

    Update

--- shell/aliases/aliases.sh
+++ shell/aliases/aliases.sh
@@ -3,6 +3,24 @@ alias dc='cd'
 alias ..='cd ..'
 alias history='fc -l 1'
 alias sl='ls'
+alias php56='sudo brew services stop php70; brew unlink php70; brew link php56; sudo brew services start php56; valet install'
+alias php70='sudo brew services stop php56; brew unlink php56; brew link php70; sudo brew services start php70; valet install'
+
+notify() {
+    msg=${1:=Done}
+
+    osascript -e "display notification \"$msg\" with title \"Notification\""
+
+    (curl -X POST -H "Accept: application/json" -H "Content-type: application/json" \
+      -d "{\"value1\": \"$msg\"}" \
+        'https://maker.ifttt.com/trigger/ios_push/with/key/MnW8Y6xzA1GQWotOIVOi2' \
+          >/dev/null 2>&1 &)
+    say "$msg"
+}
+
+lsbucket() {
+    aws s3 ls ${1} --recursive --summarize --human-readable > ${1}.txt
+}   
 
 # source dotfiles
 reload() {
